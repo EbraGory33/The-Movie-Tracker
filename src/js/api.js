e7841d5bfa247e34f59c85e6f96c3f6a26f845f9
EbraGory33 <96397436+EbraGory33@users.noreply.github.com>
2025-09-30 13:11:44 -0400

    first commit

---
+++ src/js/api.js
@@ -0,0 +1,2 @@
+const API_KEY = "VEeE5Lye8xN9Wxmxq1VwQmcCbKsDIMc2xwUxgKE8";
+const BASE_URL = "https://api.watchmode.com/v1";
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", async () => {});
